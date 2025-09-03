60b23d782b121d4a177665007136ea059b2f2d73
Tonia Joseph <tonia.joseph@reflectionsinfos.com>
2025-09-03 19:07:22 +0530

    update config

--- config/default.js
+++ config/default.js
@@ -1,11 +1,12 @@
 module.exports = {
   port: process.env.PORT,
-  origin: "/",
+  origin: process.env.ORIGIN,
   dbUri: process.env.DBURI,
   saltWorkFactor: process.env.SALTWORKFACTOR,
   accessTokenTtl: process.env.ACCESSTOKENTTL,
   refreshTokenTtl: process.env.REFRESHTOKENTTL,
   googleClientId: process.env.GOOGLECLIENTID,
   googleClientSecret: process.env.GOOGLECLIENTSECRET,
-  googleOAuthRedirectUrl: "/api/sessions/oauth/google",
+  googleOAuthRedirectUrl:
+    "https://restapi-server-i4ry.onrender.com/api/sessions/oauth/google",
 };
